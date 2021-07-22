a8992eaed1a061788eebc7850702342c90b4dfed
Dev-hoon <asdnm5030@naver.com>
2021-07-22 20:08:33 +0900

    배열 사용법 익히기

---
+++ src/array.js
@@ -0,0 +1,12 @@
+const array = [1, 2, 3, 4, 5];
+console.log(array[7]);
+
+const objects = [{ name: "멍멍이" }, { name: "야옹이" }];
+console.log(objects);
+console.log(objects[0]);
+console.log(objects[0].name);
+
+objects.push({ name: "짹짹이" });
+console.log(objects);
+console.log(objects[2]);
+console.log(objects.length);
