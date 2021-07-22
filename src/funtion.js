73cc9d016f319f482663ad86c93b080abffadc2a
Dev-hoon <asdnm5030@naver.com>
2021-07-22 20:01:59 +0900

    화살표 함수 사용방법

--- src/funtion.js
+++ src/funtion.js
@@ -7,8 +7,13 @@ function add(a, b) {
   //console.log("해당 부분은 실행이 되지 않습니다.!");
 }
 
-const result = add(1, 2);
-console.log(result);
+const add2 = (a, b) => {
+  return a + b;
+};
+
+console.log(add2(5, 13));
+
+console.log(add(1, 2));
 
 function hello(name) {
   console.log("안녕 " + name + "아!!!!");
